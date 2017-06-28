a8dcb1b026a57f4fd3b41d0c67b51d78317245d9
wadahana <wadahana@gmail.com>
2017-06-28 10:12:05 +0800

    README add user guide

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 吴昕 on 27/06/2017.
+//  Copyright © 2017 ChinaNetCenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by 吴昕 on 27/06/2017.
+//  Copyright © 2017 ChinaNetCenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
